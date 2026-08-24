6e5380e4f2b4b80803e989e4256b0fad38e62ba1
DineshKumar <dineshkumarkb@gmail.com>
2026-08-24 18:29:37 +0530

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        curr_sum = 0
+
+        res = nums[0]
+
+        for i in range(len(nums)):
+
+            curr_sum += nums[i]
+
+            res = max(res, curr_sum)
+
+            if curr_sum < 0:
+                curr_sum = 0
+
+
+        return res
+
+            
+
+        
